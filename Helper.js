29e32eaae6dcf3942ab183e5772908cb5388679e
songxing <evan.song@gaopeng.com>
2017-04-14 15:39:01 +0800

    添加ajax

--- Helper.js
+++ Helper.js
@@ -33,5 +33,68 @@ var Helper = {
             }
         }
         return source;
+    },
+
+    ajax: function (opt) {
+        var options = {
+            url: '',
+            method: 'get',
+            sync: true,
+            dataType: 'json', // ['json', xml, text]
+            data: {},
+            success: function () {},
+            error: function () {}
+        }
+
+        this.deepClone(options, opt);
+
+        var xmlhttp;
+        if (window.XMLHttpRequest) {
+            //  IE7+, Firefox, Chrome, Opera, Safari 浏览器执行代码
+            xmlhttp = new XMLHttpRequest();
+        }
+        else {
+            // IE6, IE5 浏览器执行代码
+            xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+        }
+
+        var method = options.method.toUpperCase();
+        var data = '';
+        for (var key in options.data) {
+            data += key + '=' + (options.data[key] !== undefined ? options.data[key] : '') + '&';
+        }
+        if (data.length > 0) {
+            data = data.substr(0, data.length - 1);
+        }
+
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState==4){
+                if (xmlhttp.status == 200) {
+                    var result;
+                    if (options.dataType === 'xml') {
+                        result = xmlhttp.responseXML;
+                    }
+                    else {
+                        result = xmlhttp.responseText;
+                        if (options.dataType === 'json') {
+                            result = JSON.parse(result);
+                        }
+                    }
+                    options.success(result);
+                }
+                else {
+                    options.error();
+                }
+            }
+        }
+
+        if (method === 'GET') {
+            xmlhttp.open(method, options.url + (data.length > 1 ? (options.url.indexOf('?') > -1 ? '&' : '?') : '') + data, options.sync);
+            xmlhttp.send();
+        }
+        else {
+            xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+            xmlhttp.send(data);
+        }
     }
 }
